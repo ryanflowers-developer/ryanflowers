dee64602bbaaeb68549ab611c25badcc2f8fe171
rfdev <51009900+ryanflowers-developer@users.noreply.github.com>
2020-09-01 16:01:27 -0400

    Update config.js

--- src/.temp/config.js
+++ src/.temp/config.js
@@ -1,7 +1,7 @@
 export default {
   "trailingSlash": true,
   "pathPrefix": "",
-  "titleTemplate": "%s - Ryan Flowers",
+  "titleTemplate": "%s - Portfolio",
   "siteUrl": "",
   "version": "0.7.19",
   "catchLinks": true
